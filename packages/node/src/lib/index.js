381a68f7659d3a864b0deba39777c2bc8b382e4d
pouchlabs <pouchlabs@gmail.com>
2024-12-02 21:26:41 +0300

    @cleanup entry file

--- packages/node/src/lib/index.js
+++ packages/node/src/lib/index.js
@@ -1,23 +1,4 @@
 import Pouchlite from "./lite.js";
-import { z } from "zod";
-
-// creating a schema for strings
-const mySchema = z.object({
-  name:z.string(),
-  age:z.number()
-});
-
-
-const lite = new Pouchlite({path:"./"}) 
-  //provide valid path to file or leave blank for default
- const usersdb = lite.useDb("users") //dbname
- const posts = usersdb.use("posts");  
-console.log(lite)
- //posts.defineSchema(mySchema)  
-
-//usersdb.removeCollection("posts")  
-//todo remove function
-
 export default Pouchlite;
 export { 
     Pouchlite
